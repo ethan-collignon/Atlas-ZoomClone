f4aeb8aa75997cc51aad10e72c647bee344a040c
Ethan Collignon <ethan.collignon@gmail.com>
2022-01-14 15:11:56 -0500

    bug when trying to load in 2nd stream. Cannot figure it out. Server just wants to crash after page load

--- public/script.js
+++ public/script.js
@@ -1,7 +1,13 @@
+const socket = io('/');
 const videoGrid = document.getElementById('video-grid');
 const myVideo = document.createElement('video');
 myVideo.muted = true;
 
+let peer = new Peer(undefined, {
+    path: '/peerjs',
+    host: '/',
+    port: '3030'
+})
 
 let myVideoStream
 navigator.mediaDevices.getUserMedia({
@@ -10,9 +16,36 @@ navigator.mediaDevices.getUserMedia({
 }).then(stream => {
     myVideoStream = stream;
     addVideoStream(myVideo, stream);
+
+    peer.on('call', call => {
+        call.answer(stream)
+        const video = document.createElement('video')
+        call.on('stream', userVideoStream => {
+            addVideoStream(video, userVideoStream)
+        })
+    })
+     
+    socket.on('user-connected', (userId) => {
+        connectToNewUser(userId, stream);
+    })
+})
+
+peer.on('open', id => {
+    socket.emit('join-room', ROOM_ID, id);
 })
 
 
+
+const connectToNewUser = (userId, stream) => {
+    const call = peer.call(userId, stream)
+    const video = document.createElement('video')
+    call.on('stream', userVideoStream => {
+        addVideoStream(video, userVideoStream)
+    })
+}
+
+
+ 
 const addVideoStream = (video, stream) => {
     video.srcObject = stream;
     video.addEventListener('loadedmetadata', () => { 
